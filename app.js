0f7d7ba258bd703bb41028bf176d2cd3a330fed4
Cyberfox206 <bbrontesi@edenschool.fr>
2023-06-27 09:42:17 +0200

    add one animation on the card

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let cli = document.getElementById("conter");
+let all_card = document.getElementById("card");
 let cont = 0;
+
 cli.addEventListener("click", () => {
-    cont++;
-    cli.textContent = `${cont}`;
+    all_card.classList.add("animation");
+    setTimeout(() => {
+        all_card.classList.remove("animation");
+        cont++;
+        cli.textContent = `${cont}`;
+    }, 3 * 1000);
 })
